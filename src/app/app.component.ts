b460b81e8d828f466a8849cc5c90e9e9ff661b33
wojtekzalewski <tomaszwojtekzalewski@gmail.com>
2025-05-27 17:17:11 +0200

    listy; dodwanie usuwanie taskow - zrobione, licznik

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
+import { NgFor, NgIf} from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgFor, NgIf],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'todo';
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+  newTask: string;
+  tasksList: Array<string> = [];
+  tasksDone: Array<string> = [];
+  add() {
+    this.tasksList.push(this.newTask);
+    this.newTask = '';
+    console.log(this.tasksList);
+  }
+
+  remove(task: string) {
+    this.tasksList = this.tasksList.filter((e) => e !== task);
+  }
+
+  done(task: string) {
+    this.tasksDone.push(task);
+    this.remove(task);
+    console.log(this.tasksList);
+  }
 }
